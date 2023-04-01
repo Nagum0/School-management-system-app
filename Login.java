c6fa2b69126e0e84bd0ffcb43e8f8641b100a87f
zeki <121572632+Nagum0@users.noreply.github.com>
2023-04-01 21:09:10 +0200

    Add files via upload

--- Login.java
+++ Login.java
@@ -2,12 +2,19 @@ package school_magement_app;
 
 import java.awt.Font;
 import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Login extends JPanel {
+public class Login extends JPanel implements ActionListener{
     int width;
     int height;
+    Label loginLbl;
+    JButton loginBtn;
+    private HashMap<String, String> userLoginData;
 
     //Login Contructor
     public Login(int width, int height) {
@@ -19,8 +26,38 @@ public class Login extends JPanel {
         this.setBounds(0, 0, this.width, this.height);
 
         //Login title label
-        Label loginLbl = new Label("Login");
+        loginLbl = new Label("Login");
         loginLbl.setFont(new Font(Settings.FONT, Font.BOLD, Settings.TITLE_FONT_SIZE));
         this.add(loginLbl);
+
+        //Login button
+        loginBtn = new JButton();
+        loginBtn.addActionListener(this); //Setup actionlistener
+        loginBtn.setText("Log in");
+        loginBtn.setFont(new Font(Settings.FONT, Font.BOLD, Settings.FONT_SIZE));
+        loginBtn.setFocusable(false);
+        this.add(loginBtn);
+    }
+
+    //Login
+    private void login() {
+        System.out.println("loginBtn clicked");
+        readLoginData();
+    }
+
+    //Get login data from txt file
+    private void readLoginData() {
+        userLoginData = new HashMap<String, String>();
+        userLoginData.put("admin00", "0000");
+
+        System.out.println(userLoginData.get("admin00"));
+    }
+
+    //Actionlistener
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginBtn) {
+            login();
+        }
     }
 }
--- Settings.java
+++ Settings.java
@@ -1,9 +1,32 @@
 package school_magement_app;
 
+import java.util.HashMap;
+
 public class Settings {
     public static int WIDTH = 800;
     public static int HEIGHT = 700; 
     public static String FONT = "Calibri";
     public static int FONT_SIZE = 20;
     public static int TITLE_FONT_SIZE = 25;
+
+    //Test accounts hashmap
+    private static HashMap<String, String> dict;
+
+    //Test stuff---------------------------------
+    public static void main(String[] args) {
+        String user = "admin00";
+        String pass = "0000";
+
+        dict = new HashMap<String, String>();
+        dict.put("admin00", "0000");
+        dict.put("student00", "1111");
+        dict.put("teach00", "2222");
+
+        if (pass == dict.get(user)) {
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+    }   
 }
