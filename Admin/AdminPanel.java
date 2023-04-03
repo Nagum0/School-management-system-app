900823c911ac87579385488e2f0c5a84e77e7aeb
zeki <121572632+Nagum0@users.noreply.github.com>
2023-04-03 21:31:36 +0200

    Add files via upload

--- Admin/AdminPanel.java
+++ Admin/AdminPanel.java
@@ -1,14 +1,55 @@
 package App.Admin;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import App.Main;
 import App.Settings;
+
+//Class for buttons that open admin frames
+class AddBtn extends JButton implements ActionListener{
+    String openNewWindow;
+
+    //Constructor
+    public AddBtn(String text, int x, int y, String openNewWindow) {
+        this.openNewWindow = openNewWindow;
+
+        this.setText(text);
+        this.setBounds(x, y, Settings.MENU_BTN_WIDTH, Settings.MENU_BTN_HEIGHT);
+        this.setFocusable(false);
+        this.addActionListener(this);
+    }
+
+    //Button actionlistener
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this) {
+            //Switch for opening new admin frames
+            switch (openNewWindow) {
+                //Open student frame
+                case "add_student_frame":
+                    Settings.nextPanel = "add_student";
+                    break;
+                //Open teacher frame
+                case "add_teacher_frame":
+                    Settings.nextPanel = "add_teacher";
+                    break;
+            }
+
+            //Change panel method called
+            Main.changePanel();
+        }
+    }
+}
 
 //Menu for admin
 public class AdminPanel extends JPanel{
     //Menu buttons
-    JButton openAddStudent;
+    AddBtn openAddStudentBtn;
+    AddBtn openAddTeacherBtn;
     
     //Constructor 
     public AdminPanel() {
@@ -16,12 +57,11 @@ public class AdminPanel extends JPanel{
         this.setLayout(null);
         this.setVisible(true);
 
-        //Open add student frame
-        openAddStudent = new JButton("Add student");
-        openAddStudent.setFocusable(false);
-        openAddStudent.setBounds(0, 0, Settings.MENU_BTN_WIDTH, Settings.MENU_BTN_HEIGHT);
+        openAddStudentBtn = new AddBtn("Add student", 170, 125, "add_student_frame");
+        openAddTeacherBtn = new AddBtn("Add teacher", 470, 125, "add_teacher_frame");
 
         //Adding the widgets
-        this.add(openAddStudent);
+        this.add(openAddStudentBtn);
+        this.add(openAddTeacherBtn);
     }
 }
