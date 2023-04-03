47c071d94a9a310faf762145cadbf0576e22fa5d
zeki <121572632+Nagum0@users.noreply.github.com>
2023-04-03 21:30:37 +0200

    Add files via upload

---
+++ AddStudent.java
@@ -0,0 +1,8 @@
+package App.Admin;
+
+public class AddStudent {
+    
+    public AddStudent() {
+        System.out.println("Add student");
+    }
+}
--- AdminPanel.java
+++ AdminPanel.java
@@ -6,21 +6,50 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import App.Main;
 import App.Settings;
 
-class AddBtn extends JButton{
+//Class for buttons that open admin frames
+class AddBtn extends JButton implements ActionListener{
+    String openNewWindow;
 
     //Constructor
-    public AddBtn(String text, int x, int y) {
-        this.addActionListener(actionListener);
+    public AddBtn(String text, int x, int y, String openNewWindow) {
+        this.openNewWindow = openNewWindow;
+
+        this.setText(text);
+        this.setBounds(x, y, Settings.MENU_BTN_WIDTH, Settings.MENU_BTN_HEIGHT);
+        this.setFocusable(false);
+        this.addActionListener(this);
+    }
+
+    //Button actionlistener
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this) {
+            //Switch for opening new admin frames
+            switch (openNewWindow) {
+                //Open student frame
+                case "add_student_frame":
+                    Settings.nextPanel = "add_student";
+                    break;
+                //Open teacher frame
+                case "add_teacher_frame":
+                    Settings.nextPanel = "add_teacher";
+                    break;
+            }
+
+            //Change panel method called
+            Main.changePanel();
+        }
     }
 }
 
 //Menu for admin
-public class AdminPanel extends JPanel implements ActionListener{
+public class AdminPanel extends JPanel{
     //Menu buttons
-    JButton openAddStudentBtn;
-    JButton openAddTeacherBtn;
+    AddBtn openAddStudentBtn;
+    AddBtn openAddTeacherBtn;
     
     //Constructor 
     public AdminPanel() {
@@ -28,28 +57,11 @@ public class AdminPanel extends JPanel implements ActionListener{
         this.setLayout(null);
         this.setVisible(true);
 
-        //Open add student frame
-        openAddStudentBtn = new JButton("Add student");
-        openAddStudentBtn.addActionListener(this);
-        openAddStudentBtn.setFocusable(false);
-        openAddStudentBtn.setBounds(150, 125, Settings.MENU_BTN_WIDTH, Settings.MENU_BTN_HEIGHT);
+        openAddStudentBtn = new AddBtn("Add student", 170, 125, "add_student_frame");
+        openAddTeacherBtn = new AddBtn("Add teacher", 470, 125, "add_teacher_frame");
 
         //Adding the widgets
         this.add(openAddStudentBtn);
-    }
-
-    //Button methods
-    //Open add student frame btn method
-    private void openAddStudentFrame() {
-        System.out.println("btn-1 clicked");
-    }
-
-    //Button actionlistener
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        //Open add student frame
-        if (e.getSource() == openAddStudentBtn) {
-            openAddStudentFrame();
-        }
+        this.add(openAddTeacherBtn);
     }
 }
