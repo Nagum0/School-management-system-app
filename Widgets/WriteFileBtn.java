e9e5b08326c3068a3a734bc3a44a5f5ff31827ef
zeki <121572632+Nagum0@users.noreply.github.com>
2023-04-04 17:46:14 +0200

    Add files via upload

---
+++ Widgets/WriteFileBtn.java
@@ -0,0 +1,18 @@
+package App.Widgets;
+
+import javax.swing.JButton;
+
+import App.Settings;
+import App.Admin.AddStudent;
+
+//Write file btn class
+public class WriteFileBtn extends JButton{
+    
+    //Constructor
+    public WriteFileBtn(int x, int y, String text, AddStudent that) {
+        this.setBounds(x, y, 200, Settings.BTN_HEIGHT);
+        this.setFocusable(false);
+        this.addActionListener(that);
+        this.setText(text);
+    }
+}
