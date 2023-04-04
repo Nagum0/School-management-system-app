94ab767afe75307bdb8b6a5a59eba23738eacd63
zeki <121572632+Nagum0@users.noreply.github.com>
2023-04-04 18:27:10 +0200

    Add files via upload

--- Admin/AddStudent.java
+++ Admin/AddStudent.java
@@ -4,28 +4,41 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JTextField;
 
 import App.Settings;
 import App.Widgets.EntryField;
+import App.Widgets.EntryLabel;
 import App.Widgets.WriteFileBtn;
 
 //Add student to data base -> JFrame
-public class AddStudent extends JFrame implements ActionListener{
-    //Widgets
+public class AddStudent extends JFrame implements ActionListener {
+    //Widgets----------------------------------------------------
+    //Labels
+    EntryLabel firstNameLbl;
+    EntryLabel lastNameLbl;
+    EntryLabel ageLbl;
+    EntryLabel gradeLbl;
+    EntryLabel avgGradeLbl;
+    EntryLabel specSubject1Lbl;
+    EntryLabel specSubject2Lbl;
+
+    //Entryfields
     EntryField firstNameField;
-    JTextField lastNameField;
-    JTextField ageField;
-    JTextField gradeField;
-    JTextField avgGrade;
-    JTextField specSubject1;
-    JTextField specSubject2;
+    EntryField lastNameField;
+    EntryField ageField;
+    EntryField gradeField;
+    EntryField avgGrade;
+    EntryField specSubject1;
+    EntryField specSubject2;
+
+    //Buttons
     WriteFileBtn addStudentBtn;
 
     //Constructor
     public AddStudent() {
         //Window setup
         this.setSize(Settings.WIDTH, Settings.HEIGHT);
+        this.setResizable(false);
         this.setTitle("Add Student");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //This is temporary for the testing. Turn off later.
         this.setLocationRelativeTo(null); //Sets opening position to the center of the screen.
@@ -33,13 +46,40 @@ public class AddStudent extends JFrame implements ActionListener{
         this.setVisible(true);
 
         //Settng up widgets
-        firstNameField = new EntryField(50, 50);
+        firstNameLbl = new EntryLabel("First name: ", 250, 80);
+        firstNameField = new EntryField(330, 90);
+
+        lastNameLbl = new EntryLabel("Last name: ", 250, 130);
+        lastNameField = new EntryField(330, 140);
+
+        ageLbl = new EntryLabel("Age: ", 270, 180);
+        ageField = new EntryField(330, 190);
+
+        gradeLbl = new EntryLabel("Grade: ", 270, 230);
+        gradeField = new EntryField(330, 240);
+
+        avgGradeLbl = new EntryLabel("Avg grade: ", 250, 280);
+        avgGrade = new EntryField(330, 290);
 
         //Add student btn
-        addStudentBtn = new WriteFileBtn(50, 100, "Add student", this);
+        addStudentBtn = new WriteFileBtn(300, 600, "Add student", this);
 
         //Adding widgets
+        this.add(firstNameLbl);
         this.add(firstNameField);
+
+        this.add(lastNameLbl);
+        this.add(lastNameField);
+
+        this.add(ageLbl);
+        this.add(ageField);
+        
+        this.add(gradeLbl);
+        this.add(gradeField);
+
+        this.add(avgGradeLbl);
+        this.add(avgGrade);
+
         this.add(addStudentBtn);
     }
 
@@ -48,8 +88,10 @@ public class AddStudent extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == addStudentBtn) {
             System.out.println(firstNameField.returnValue());
+            System.out.println(lastNameField.returnValue());
         }
     }
+
     //Test run for add_student
     public static void main(String[] args) {
         new AddStudent();
