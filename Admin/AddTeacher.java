0b8a2e2e3b8bab4f11936337745221f89e0b9c1b
zeki <121572632+Nagum0@users.noreply.github.com>
2023-04-06 19:25:42 +0200

    Add files via upload

--- Admin/AddTeacher.java
+++ Admin/AddTeacher.java
@@ -12,41 +12,42 @@ import App.Widgets.EntryField;
 import App.Widgets.EntryLabel;
 import App.Widgets.WriteFileBtn;
 
-//Add student to data base -> JFrame
-public class AddStudent extends JFrame implements ActionListener {
+//add teacher frame
+public class AddTeacher extends JFrame implements ActionListener{
     //Widgets----------------------------------------------------
     //Labels
     EntryLabel firstNameLbl;
     EntryLabel lastNameLbl;
     EntryLabel ageLbl;
-    EntryLabel gradeLbl;
-    EntryLabel avgGradeLbl;
-    EntryLabel specSubject1Lbl;
-    EntryLabel specSubject2Lbl;
+    EntryLabel titleLbl;
+    EntryLabel subject1Lbl;
+    EntryLabel subject2Lbl;
+    EntryLabel yearsTaughtLbl;
 
     //Entryfields
     EntryField firstNameField;
     EntryField lastNameField;
     EntryField ageField;
-    EntryField gradeField;
-    EntryField avgGrade;
-    EntryField specSubject1;
-    EntryField specSubject2;
+    EntryField titleField;
+    EntryField subject1Field;
+    EntryField subject2Field;
+    EntryField yearsTaughtField;
 
     //Buttons
-    WriteFileBtn addStudentBtn;
+    WriteFileBtn addTeacherBtn;
 
     //Constructor
-    public AddStudent() {
+    public AddTeacher() {
         //Window setup
         this.setSize(Settings.WIDTH, Settings.HEIGHT);
         this.setResizable(false);
         this.setTitle("Add Student");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Temporary for testing
         this.setLocationRelativeTo(null); //Sets opening position to the center of the screen.
         this.setLayout(null);
         this.setVisible(true);
 
-        //Settng up widgets
+        //Widget setup
         firstNameLbl = new EntryLabel("First name: ", 250, 80);
         firstNameField = new EntryField(330, 90);
 
@@ -56,21 +57,21 @@ public class AddStudent extends JFrame implements ActionListener {
         ageLbl = new EntryLabel("Age: ", 270, 180);
         ageField = new EntryField(330, 190);
 
-        gradeLbl = new EntryLabel("Grade: ", 270, 230);
-        gradeField = new EntryField(330, 240);
+        titleLbl = new EntryLabel("Title: ", 270, 230);
+        titleField = new EntryField(330, 240);
 
-        avgGradeLbl = new EntryLabel("Avg grade: ", 250, 280);
-        avgGrade = new EntryField(330, 290);
+        subject1Lbl = new EntryLabel("Subject 1: ", 250, 280);
+        subject1Field = new EntryField(330, 290);
 
-        specSubject1Lbl = new EntryLabel("Spec subject 1: ", 250, 330);
-        specSubject1 = new EntryField(330, 340);
+        subject2Lbl = new EntryLabel("Subject 2: ", 250, 330);
+        subject2Field = new EntryField(330, 340);
 
-        specSubject2Lbl = new EntryLabel("Spec subject 2: ", 250, 380);
-        specSubject2 = new EntryField(330, 390);
+        yearsTaughtLbl = new EntryLabel("Years taught: ", 230, 380);
+        yearsTaughtField = new EntryField(330, 390);
 
         //Add student btn
-        addStudentBtn = new WriteFileBtn(300, 600, "Add student");
-        addStudentBtn.addActionListener(this);
+        addTeacherBtn = new WriteFileBtn(300, 600, "Add teacher");
+        addTeacherBtn.addActionListener(this);
 
         //Adding widgets
         this.add(firstNameLbl);
@@ -82,34 +83,34 @@ public class AddStudent extends JFrame implements ActionListener {
         this.add(ageLbl);
         this.add(ageField);
         
-        this.add(gradeLbl);
-        this.add(gradeField);
+        this.add(titleLbl);
+        this.add(titleField);
 
-        this.add(avgGradeLbl);
-        this.add(avgGrade);
+        this.add(subject1Lbl);
+        this.add(subject1Field);
 
-        this.add(specSubject1Lbl);
-        this.add(specSubject1);
+        this.add(subject2Lbl);
+        this.add(subject2Field);
 
-        this.add(specSubject2Lbl);
-        this.add(specSubject2);
+        this.add(yearsTaughtLbl);
+        this.add(yearsTaughtField);
 
-        this.add(addStudentBtn);
+        this.add(addTeacherBtn);
     }
 
-    //Button actionlistener
+    //Action listeners
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == addStudentBtn) {
+        if (e.getSource() == addTeacherBtn) {
             //Writing student files
-            try (FileWriter writer = new FileWriter(String.format("C:/Users/csabe/OneDrive/Desktop/school_magement_app/files/Students/%s%s.txt", firstNameField.returnValue(), lastNameField.returnValue()))) {
+            try (FileWriter writer = new FileWriter(String.format("C:/Users/csabe/OneDrive/Desktop/school_magement_app/files/Teachers/%s%s.txt", firstNameField.returnValue(), lastNameField.returnValue()))) {
                 writer.write(String.format("First name: %s\n", firstNameField.returnValue()));
                 writer.write(String.format("Last name: %s\n", lastNameField.returnValue()));
                 writer.write(String.format("Age: %s\n", ageField.returnValue()));
-                writer.write(String.format("Grade: %s\n", gradeField.returnValue()));
-                writer.write(String.format("Avg grade: %s\n", avgGrade.returnValue()));
-                writer.write(String.format("Spec subject 1: %s\n", specSubject1.returnValue()));
-                writer.write(String.format("Spec subject 2: %s", specSubject2.returnValue()));
+                writer.write(String.format("Title: %s\n", titleField.returnValue()));
+                writer.write(String.format("Subject 1: %s\n", subject1Field.returnValue()));
+                writer.write(String.format("Subject 2: %s\n", subject2Field.returnValue()));
+                writer.write(String.format("Years taught: %s", yearsTaughtField.returnValue()));
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
